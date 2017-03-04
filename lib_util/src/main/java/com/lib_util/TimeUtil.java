37ae99845d8b3f57111b20c2e607326dccc09756
Rui Zhang <ruizhang81@gmail.com>
2017-03-04 11:46:10 +0800

    0.962

--- lib_util/src/main/java/com/lib_util/TimeUtil.java
+++ lib_util/src/main/java/com/lib_util/TimeUtil.java
@@ -1,5 +1,8 @@
 package com.lib_util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by zhangrui on 17/1/5.
  */
@@ -7,4 +10,13 @@ package com.lib_util;
 public class TimeUtil {
 
 
+    public static long getTimeStemp(String time,String timeFormat){
+        try{
+            Date date =  new SimpleDateFormat(timeFormat).parse(time);
+            return date.getTime()/1000;
+        }catch (Exception e){
+
+        }
+        return 0;
+    }
 }
