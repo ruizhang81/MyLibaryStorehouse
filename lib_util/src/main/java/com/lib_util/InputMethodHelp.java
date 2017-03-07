dd54d115d1260e4f7975bccc1460b6194d804e3e
Rui Zhang <ruizhang81@gmail.com>
2017-03-07 16:04:23 +0800

    0.966

--- lib_util/src/main/java/com/lib_util/InputMethodHelp.java
+++ lib_util/src/main/java/com/lib_util/InputMethodHelp.java
@@ -6,17 +6,26 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by zhangrui on 17/3/7.
  */
 
 public class InputMethodHelp {
 
-    public static void show(EditText edittext,Context context){
+    public static void show(final EditText edittext,final Context context){
         edittext.requestFocus();
-        edittext.setFocusable(true);
-        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.showSoftInputFromInputMethod(edittext.getWindowToken(),0);
+        Timer timer = new Timer(); //设置定时器
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() { //弹出软键盘的代码
+                InputMethodManager imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(edittext, InputMethodManager.RESULT_SHOWN);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.HIDE_IMPLICIT_ONLY);
+            }
+        }, 300); //设置300毫秒的时长
     }
 
     public static void hide(EditText edittext,Context context){
